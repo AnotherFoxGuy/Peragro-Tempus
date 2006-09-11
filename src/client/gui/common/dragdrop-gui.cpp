36487a6a266bd336864eb3530d431cbf9b151f6b
sueastside <>
2006-09-11 14:32:10 +0000

    - Sueastside prepared some things for the GUI cleanup.

---
+++ src/client/gui/common/dragdrop-gui.cpp
@@ -0,0 +1,36 @@
+/*
+    Copyright (C) 2005 Development Team of Peragro Tempus
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include "client/gui/common/dragdrop-gui.h"
+
+#include "CEGUI.h"
+#include "CEGUIWindowManager.h" 
+#include "CEGUILogger.h"
+
+#include "client/network/network.h"
+#include "client/gui/guimanager.h"
+
+
+DragDrop::DragDrop (GUIManager* guimanager)
+: GUIWindow (guimanager)
+{
+}
+
+DragDrop::~DragDrop ()
+{
+}
