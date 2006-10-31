485e0231bd42e914cd6a2c1fad45bf2fa3642b3f
sueastside <>
2006-10-31 17:50:29 +0000

    Sueastside committed Sell window. Several jam file fixes.

---
+++ src/client/gui/sell-gui.h
@@ -0,0 +1,53 @@
+/*
+    Copyright (C) 2005 Development Team of Peragro Tempus
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General public License for more details.
+
+    You should have received a copy of the GNU General public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#ifndef SELL_GUI_H
+#define SELL_GUI_H
+
+#include "base-gui.h"
+
+#include "client/gui/common/dragdrop-gui.h"
+#include "client/gui/common/slot.h"
+
+class SellWindow : public GUIWindow
+{
+private:
+  DragDrop* dragdrop;
+  ItemMGR* itemmanager;
+
+  csArray<Slot*> upperslots;
+  csArray<Slot*> lowerslots;
+
+private:
+  bool OnCloseButton(const CEGUI::EventArgs& args);
+  bool OnAccept(const CEGUI::EventArgs& args);
+
+public:
+  SellWindow(GUIManager* guimanager);
+  virtual ~SellWindow();
+  void CreateGUIWindow();    // load the chat guilayout and register button events.
+
+public:
+  bool AddItem(unsigned int itemid, unsigned int slotid);
+  void CancelTrade();
+  void AcceptTrade();
+
+};
+
+
+#endif // SELL_GUI_H
