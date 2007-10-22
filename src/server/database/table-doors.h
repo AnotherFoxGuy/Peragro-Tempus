b9a1dca630acf1821114c6e422cab67248abb5d9
swedishcoder <>
2007-10-22 00:44:45 +0000

    Adding doxygen comments

--- src/server/database/table-doors.h
+++ src/server/database/table-doors.h
@@ -25,34 +25,105 @@ class ResultSet;
 #include "common/util/ptstring.h"
 #include "table.h"
 
+
+/**
+ * Class holding data associated with a door.
+ */
 class DoorsTableVO
 {
 public:
+  /// The id of the door.
   int id;
+  /// The name of the door.
   ptString name;
+  /// The locked state.
   int islocked;
+  /// The open state.
   int isopen;
 };
 
 class DoorsTable : public Table
 {
 private:
+  /**
+   * Parses a single database result set.
+   * The caller is responsible for freeing the returned object.
+   * @param rs The database result set.
+   * @param row The row in the result table to use, default row is 0.
+   * @return Pointer to DoorsTableVO which holds all data associated with door.
+   */ 
   DoorsTableVO* parseSingleResultSet(ResultSet* rs, size_t row = 0);
+ 
+  /**
+   * Will parse all data return from the database query and insert it into
+   * an array.
+   * The caller is responsible for freeing the objects in the returned array.
+   * @param rs The result set containing the database query result.
+   * @return An array containing all DoorsTableVO found in the Result set.
+   */
   Array<DoorsTableVO*> parseMultiResultSet(ResultSet* rs);
 public:
+  /**
+   * Constructor for DoorsTable class.
+   * If database is empty, the createTable function will be called.
+   * @param db Pointer to the database.
+   */
   DoorsTable(Database* db);
-
+  /**
+   * Creates a table in the database that will store doors table.
+   */
   void createTable();
+  /**
+   * Removes all doors table from the database.
+   */
   void dropTable();
-
+  /**
+   * Insert a door table into the database.
+   * @param vo The doors table to insert.
+   */
   void insert(DoorsTableVO* vo);
+  /**
+   * Removes a door from the database.
+   * @param id The id of the door to remove.
+   */
   void remove(int id);
-
+  /**
+   * Returns the number of door tables stored in the database.
+   * @return The amount of door tables.
+   */
   unsigned int getCount();
+  /**
+   * Checks if a door table exists in the database.
+   * @param name The name of the door table to search for.
+   * @return True if the door table is found, otherwise false.
+   */
   bool existsByName(ptString name);
+  /**
+   * Checks if a door table exists in the database.
+   * @param id The ID of the door table to search for.
+   * @return True if the door table is found, otherwise false.
+   */
   bool existsById(int id);
+  /**
+   * Returns a door table given its name.
+   * The caller is responsible for freeing the returned door table.
+   * @param name The name of the door table to search for.
+   * @return The door table VO class, or 0 if not found.
+   */
   DoorsTableVO* getByName(ptString name);
+  /**
+   * Returns a door table given its name.
+   * The caller is responsible for freeing the returned door table.
+   * @param id The ID of the door table to search for.
+   * @return The door table VO class, or 0 if not found.
+   */
   DoorsTableVO* getById(int id);
+  /**
+   * This function will load all door tables from the database 
+   * and return an array containing them.
+   * The caller is responsible for freeing all door tables in the array.
+   * @return An array containing all available door table VO's.
+   */
   Array<DoorsTableVO*> getAll();
 };
 
