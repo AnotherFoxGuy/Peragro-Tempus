d44d995e94866926c153610594e70f4009126f70
cyanox <>
2008-03-06 21:17:28 +0000

    Fixed minor bash oddity.

--- scripts/regenApiDocsCron.sh
+++ scripts/regenApiDocsCron.sh
@@ -8,7 +8,7 @@ svn up
 # ./autogen.sh
 # echo "--> Configuring source tree."
 # ./configure --without-cs --without-cel --without-CEGUI --without-pthread --without-boost
-export PTREV=`svn info|grep Revision`
+export PTREV="`svn info|grep Revision`"
 echo "--> Generating project files."
 jam apidocs
 echo "--> Copying generated files to website."
