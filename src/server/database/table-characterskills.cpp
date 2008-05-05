5800c4f0089f7e7eb975cb73c21115eb3c039110
swedishcoder <>
2008-05-05 20:43:45 +0000

    Added some doxygen comments

--- src/server/database/table-characterskills.cpp
+++ src/server/database/table-characterskills.cpp
@@ -25,16 +25,28 @@
 
 #include "table-characterskills.h"
 
-CharacterSkillsTable::CharacterSkillsTable(Database* db) : Table(db)
-{
+/**
+ * Constructor that will create the character skill database table if it does
+ * not already exist in the database and store the database pointer for
+ * later usage.
+ *
+ * @param db The database to use for storing character skills.
+ *
+ */
+CharacterSkillsTable::CharacterSkillsTable(Database* db) : Table(db) {
   ResultSet* rs = db->query("select count(*) from characterskill;");
-  if (rs == 0)
-  {
-    createTable();
-  }
-  delete rs;
+  if (rs == 0) 
+  { 
+    createTable(); 
+  } 
+  delete rs; 
 }
 
+/**
+ * Method that creates the character skills table in the database.
+ *
+ * @todo Don't we need the level as well?
+ */
 void CharacterSkillsTable::createTable()
 {
   printf("Creating Table characterskill...\n");
@@ -44,21 +56,45 @@ void CharacterSkillsTable::createTable()
     "PRIMARY KEY (id, skill) );");
 }
 
-void CharacterSkillsTable::insert(int characterskill, int skill)
+/**
+ * Method that will do the actual database inseration for a character skill.
+ *
+ * @todo Looks like whats being stored is characterID and skillID, but when
+ * reading back "level" is read as well... Something wrong here?
+ *
+ * @param characterID The id of the character that owns the skill.
+ * @param skillID The id of the skill to insert.
+ */
+void CharacterSkillsTable::insert(int characterID, int skillID)
 {
   db->update("insert or replace into characterskill (id, skill) values ('%d','%d');",
-    characterskill, skill);
+    characterID, skillID);
 }
 
-void CharacterSkillsTable::set(int characterskill, unsigned int skill_id)
+/**
+ * Method that will set the level of a specific skill for a character.
+ *
+ * @todo Looks like whats being stored is characterID and skillID, but when
+ * reading back "level" is read as well... Something wrong here?
+ *
+ * @param characterID The id of the character for which to return the level.
+ * @param skill_id The id of the skill for which to return the level.
+ */
+void CharacterSkillsTable::set(int characterID, unsigned int skill_id)
 {
-  insert(characterskill, skill_id);
+  insert(characterID, skill_id);
 }
 
-int CharacterSkillsTable::get(int characterskill, unsigned int skill_id)
+/**
+ * Function that will return the level of a specific skill for a character.
+ *
+ * @param characterID The id of the character for which to return the level.
+ * @param skill_id The id of the skill for which to return the level.
+ */
+int CharacterSkillsTable::get(int characterID, unsigned int skill_id)
 {
   ResultSet* rs = db->query("select * from characterskill where id = '%d' and skill = '%d';",
-    characterskill, skill_id);
+    characterID, skill_id);
 
   int level = 0;
 
@@ -80,14 +116,24 @@ int CharacterSkillsTable::get(int characterskill, unsigned int skill_id)
   return level;
 }
 
+/**
+ * Method that will remove all character skill data from the database.
+ */
 void CharacterSkillsTable::dropTable()
 {
   db->update("drop table characterskill;");
 }
 
-Array<CharSkillVO*> CharacterSkillsTable::getAllEntries(int id)
+/**
+ * Function that will return all character skill entries in the database with a
+ * certain id.
+ *
+ * @param characterID This isthe id of the characterfor which to find skills.
+ */
+Array<CharSkillVO*> CharacterSkillsTable::getAllEntries(int characterID)
 {
-  ResultSet* rs = db->query("select skill from characterskill where id = '%d';", id);
+  ResultSet* rs = db->query("select skill from characterskill where id = '%d';",
+                            characterID);
   Array<CharSkillVO*> entries;
   for (size_t i=0; rs && i<rs->GetRowCount(); i++)
   {
