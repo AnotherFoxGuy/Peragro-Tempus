f84df2c1db4f0bbb428698d8464c431619625049
vornne <>
2008-09-22 10:18:32 +0000

    Temporarily shift the npc spawn points, fix /dbg pos output.

--- src/client/chat/cmddbg.cpp
+++ src/client/chat/cmddbg.cpp
@@ -122,7 +122,7 @@ namespace PT
 
             char buffer[1024];
 
-            snprintf(buffer, 1024, "Position: %s <%f.2, %f.2, %f.2>",
+            snprintf(buffer, 1024, "Position: %s %.2f, %.2f, %.2f",
               sector->QueryObject()->GetName(), pos.x, pos.y, pos.z);
 
             chatWindow->AddMessage (buffer);
