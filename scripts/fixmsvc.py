792634dab51f42e068889f9ce65931624bd90579
sueastside <>
2008-10-01 22:02:48 +0000

    Added msvc9 projectfile fixing. (blind)

--- scripts/fixmsvc.py
+++ scripts/fixmsvc.py
@@ -53,43 +53,55 @@ class Project:
 
 	  t += "EndProject\n"
 	  return t
-	
 
-os.chdir(os.path.join(os.getcwd(), "../msvc/8/"))
-
-wksall = open('wksall.sln', 'r')
-text = wksall.read()
-wksall.close()
-
-projectssecs = re.compile("Project(.*?)EndProject\n", re.S).finditer(text)
-globalsec = re.compile("Global(.*?)EndGlobal\n", re.S).search(text)
-
-text = "Microsoft Visual Studio Solution File, Format Version 9.00\n"
-text += "# Visual C++ Express 2005\n"
-projects = {}
-
+projects = {}	  
 def GetDepProject(name):
   if projects.has_key(name):
     return projects[name]
   else:
     return None
+	
+def Fix(path, version):
+	loc = os.path.join(os.getcwd(), path)
 
-for m in projectssecs:
-  p = Project(m.group(1))
-  projects[p.name] = p
+	print
+	print "="*50
+	print "Fixing solution: "+loc+'wksall.sln'
+	print "="*50
 
-for proj, deps in projtofix:
-    if projects.has_key(proj):
-      p = projects[proj]
-      for dep in deps:
-        p.AddDep(dep)
-        print "I: Added dep", dep, "to", proj
-  
-for p in projects:  
-  text += projects[p].Generate()
-  
-text += globalsec.group()
+	wksall = open(loc+'wksall.sln', 'r')
+	text = wksall.read()
+	wksall.close()
 
-wksall = open('wksall.sln', 'w')
-wksall.write(text)
-wksall.close()
+	projectssecs = re.compile("Project(.*?)EndProject\n", re.S).finditer(text)
+	globalsec = re.compile("Global(.*?)EndGlobal\n", re.S).search(text)
+
+	if version == 8:
+		text = "Microsoft Visual Studio Solution File, Format Version 9.00\n"
+		text += "# Visual C++ Express 2005\n"
+	elif version == 9:
+		text = "Microsoft Visual Studio Solution File, Format Version 10.00\n"
+		text += "# Visual C++ Express 2008\n"
+		
+	for m in projectssecs:
+		p = Project(m.group(1))
+		projects[p.name] = p
+
+	for proj, deps in projtofix:
+		if projects.has_key(proj):
+			p = projects[proj]
+			for dep in deps:
+				p.AddDep(dep)
+				print "I: Added dep", dep, "to", proj
+
+	for p in projects:  
+		text += projects[p].Generate()
+
+	text += globalsec.group()
+
+	wksall = open(loc+'wksall.sln', 'w')
+	wksall.write(text)
+	wksall.close()
+	
+Fix("../msvc/8/", 8)
+Fix("../msvc/9/", 9)
