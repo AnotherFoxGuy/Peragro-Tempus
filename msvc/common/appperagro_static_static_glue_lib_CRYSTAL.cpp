8d79cd2a008ec0036e76110807cae18fc07da4cf
PTServerSystem <>
2008-10-13 18:03:14 +0000

    Automated MSVC project file regeneration.

--- msvc/common/appperagro_static_static_glue_lib_CRYSTAL.cpp
+++ msvc/common/appperagro_static_static_glue_lib_CRYSTAL.cpp
@@ -8,12 +8,12 @@
   { csStaticVarCleanup_csutil (p); }
   CS_DEFINE_STATIC_VARIABLE_REGISTRATION (csStaticVarCleanup_local);
 #endif
-struct CS_EXPORT_SYM _Bind_peragro_static_plugins
+struct CS_EXPORT_SYM _Bind_peragro_static_plugins_CRYSTAL
 {
-  _Bind_peragro_static_plugins ();
+  _Bind_peragro_static_plugins_CRYSTAL ();
 };
-struct _static_use { _static_use (); };
-_Bind_peragro_static_plugins::_Bind_peragro_static_plugins () {}
+struct _static_use_CRYSTAL { _static_use_CRYSTAL (); };
+_Bind_peragro_static_plugins_CRYSTAL::_Bind_peragro_static_plugins_CRYSTAL () {}
 // Needed to pull in _cs_static_use object file
-namespace { _static_use _static_use_bind; }
+namespace { _static_use_CRYSTAL _static_use_CRYSTAL_bind; }
 
