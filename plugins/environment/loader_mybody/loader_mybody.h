23671d1e9050d8554ddf958c3d3bb220abcb3ae3
recon69 <>
2008-10-25 19:40:46 +0000

    Added gpl licence to files missing them

--- plugins/environment/loader_mybody/loader_mybody.h
+++ plugins/environment/loader_mybody/loader_mybody.h
@@ -1,3 +1,20 @@
+/*
+    Copyright (C) 2008 by Mogue Carpenter
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 // file : myloaderbody.h
 
 #ifndef _H_MY_LOADER_TEMPLATE___
@@ -37,7 +54,7 @@ public:
   virtual csPtr<iBase> Parse (iDocumentNode* node, 
     iStreamSource* ss, iLoaderContext* ldr_context, iBase* context, iStringArray* failed = 0);
 
-  csRef<iMyBody> ParseBody (iDocumentNode* node, 
+  virtual csRef<iMyBody> ParseBody (iDocumentNode* node, 
                       iStreamSource* ss,
                       iLoaderContext* ldr_context,
                       iBase* context,
