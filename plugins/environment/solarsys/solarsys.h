4f74eb68ecec811fa14435bf6ac13e2682b920f9
recon69 <>
2008-10-25 19:37:37 +0000

    New solarsys object, combines iMyStarbox and iMyBody to give a background from a planets surface.

---
+++ plugins/environment/solarsys/solarsys.h
@@ -0,0 +1,106 @@
+/*
+    Copyright (C) 2008 by Mogue Carpenter
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+//File: `solarsys.h'
+	  
+#ifndef _H_SOLARSYS 
+#define _H_SOLARSYS 
+
+#include <string>
+
+#include "crystalspace.h"
+#include "isolarsys.h"
+#include "imybody.h"
+#include "imystarbox.h"
+
+
+// Implementation header
+class Solarsys : public scfImplementation1<Solarsys, iSolarsys>
+{
+private:
+  iObjectRegistry* object_reg; 
+  csRef<iEngine> engine;
+  csRef<iGraphics3D> g3d;
+  bool initialized ;
+
+  /// The render manager, cares about selecting lights+meshes to render
+  csRef<FrameBegin3DDraw> drawer;
+  csRef<FramePrinter> printer;
+  csRef<iRenderManager> rm;
+
+  /// The objects to create the background
+  csRef<iMyBody> rootbody ;
+  csRef<iMyBody> surbody ;
+  float lat;
+  float lon; 
+  csRef<iMyStarbox> starbox;
+  csRef<iCamera> startcamera;  // a fixed direction that is used to rotate stars
+  csTransform* start_trans ;
+
+  /// A pointer to the view which contains the camera.
+  csRef<iView> view;
+  csRef<iSector> sector;      // Sector should be different from you main game/map sector default solarsys
+
+//  Body* rootbody;
+  std::string solarsys_name;
+ 
+
+public:
+  Solarsys(iObjectRegistry* r); 
+  virtual ~Solarsys(); 
+  virtual bool Initialize(iObjectRegistry *object_reg);
+
+  virtual void SetName(char const* name) { solarsys_name = name; };
+  virtual void SetSector(csRef<iSector>& sector);
+  virtual void SetStartCamera (csRef<iCamera>& camera) { startcamera = camera; };
+//  virtual void SetStartTrans (csTransform* trans ){ start_trans = trans; };
+  virtual void SetRootbody (csRef<iMyBody>& body) { rootbody = body; };
+  virtual void SetLongitude(long longitude ) { lon = longitude; };
+  virtual void SetLatitude(long latitude ) { lat = latitude; };
+  virtual void SetSurfaceBody (csRef<iMyBody>& body) { surbody = body; };
+  virtual void SetStarbox (csRef<iMyStarbox>& sb ) { starbox = sb; };
+
+
+
+  virtual char const* GetName() const { return solarsys_name.c_str(); };
+  virtual iSector const* GetSector() const { return sector; };
+
+  virtual void Draw( iCamera* c );
+  virtual void Draw( iCamera* c , long ts );
+
+//  virtual void Draw( iCamera c, long ts );
+
+private:
+
+  bool CreateCamera();
+
+};
+ 
+// Factory Implementation header
+class SolarsysFactory : public scfImplementation2<SolarsysFactory, iSolarsysFactory, iComponent>
+{
+private:
+  iObjectRegistry* object_reg;
+public: 
+  SolarsysFactory (iBase* p);
+ 
+  virtual csPtr<iSolarsys> CreateObject ();
+ 
+  bool Initialize (iObjectRegistry*);
+};
+#endif 
+// _H_SOLARSYS
