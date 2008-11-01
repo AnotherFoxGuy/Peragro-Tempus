01590b0feb4981cbedaea4c00c73962a86a149ba
pk <>
2008-11-01 13:30:50 +0000

    Changing runServer.sh to prevent if from filling the server harddisk with core files

--- scripts/runServer.sh
+++ scripts/runServer.sh
@@ -2,6 +2,11 @@
 
 ulimit -c unlimited;
 
+#Prevent script from spaming core files
+if [ `find . -name "core.*" | wc -l` -ge 100 ]; then
+   exit 
+fi
+
 ./peragro-server 1>server.log 2>server.err &
 
 echo $! > peragro-server.pid                           
@@ -11,9 +16,10 @@ wait
 if [ -e peragro-server.pid ]; then
   crashtime=`date +%Y%m%d-%H.%S.%M`
   if [ -e "core.` cat peragro-server.pid `" ]; then
-    cp bin/peragro-server crashed/peragro-server.$crashtime
+    cp peragro-server crashed/peragro-server.$crashtime
     mv core* crashed/core.$crashtime
   fi
+
   $0 &
 fi
 
