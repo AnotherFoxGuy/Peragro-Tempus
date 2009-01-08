e7135d0223fba1bed3def29f49594714b16faab3
swedishcoder <>
2009-01-08 21:37:29 +0000

    First step towards supporting normal attack. Added functionality for checking if a target is attackable. Basically moving code over from old combatmanager

--- src/server/combat/interactionmanager.cpp
+++ src/server/combat/interactionmanager.cpp
@@ -47,6 +47,54 @@ void Run()
   }
 }
 
+// TODO support our own char as target? In that case don't mess up locking
+bool InteractionManager::NormalAttack(Interaction *interaction)
+{
+  unsigned int targetID = 0;
+  const Entity* entity = NULL;
+  const Character* c_char = NULL;
+
+  ptScopedMonitorable<Character>
+    lockedAttacker(interaction->character);
+
+  targetID = lockedAttacker->getTargetID();
+
+  if (targetID == 0) {
+    printf(IM "targetID 0, make sure that's not a legal one\n");
+    return false;
+  }
+
+  targetEntity = Server::getServer()->getEntityManager()->findById(targetID);
+
+  if (!targetEntity) {
+    printf(IM "Invalid target\n");
+    return false;
+  }
+
+  if (targetEntity->getType() == Entity::PlayerEntityType)
+  {
+    c_char = targetEntity->getPlayerEntity()->getCharacter();
+  }
+  else if (targetEntity->getType() == Entity::NPCEntityType)
+  {
+    c_char = targetEntity->getNpcEntity()->getCharacter();
+  }
+  else
+  {
+    // Should not happen, but do not crash on release build, since fake message
+    // could bring down the server then.
+    printf(IM "Target neither player nor npc\n");
+    return false;
+  }
+
+  ptScopedMonitorable<Character> lockedTarget(c_char);
+
+
+  if (!TargetAttackable(lockedAttacker, lockedTarget)) {
+    return false;
+  }
+}
+
 bool InteractionManager::PerformInteraction(Interaction *interaction)
 {
   if (!interaction) {
@@ -55,8 +103,69 @@ bool InteractionManager::PerformInteraction(Interaction *interaction)
 
   switch(interaction->actionID) {
     case NORMAL_ATTACK:
+      NormalAttack(interaction);
+      break;
+    case HEAL:
       break;
   }
+}
+
+bool InteractionManager::TargetAttackable(Character* lockedAttacker,
+                                          Character* lockedTarget)
+{
+  const float maxAttackDistance = 0;
+  const float distance = 0;
+  const float attackerRotation = 0;
+  const float attackAngle = 0;
+  const float angleDiff = 0;
+  static const float allowdAngle = PT_PI * 0.3f;
+
+
+
+  const PtVector3 attackerPos(attacker->getEntity()->getPos());
+  const PtVector3 targetPos(target->getEntity()->getPos());
+
+  maxAttackDistance = GetReach(lockedAttacker);
+  distance = Distance(attackerPos, targetPos);
+
+  printf(IM "attackerPos: %s, targetPos: %s, distance %f, "
+    "maxAttackDistance: %f\n", attackerPos.ToString().c_str(),
+    targetPos.ToString().c_str(), distance, maxAttackDistance);
+
+  if (distance > maxAttackDistance)
+  {
+    // Target out of reach
+    printf(IM "Target out of range\n");
+    return false;
+  }
+
+  PtVector3 difference = attackerPos - targetPos;
+
+  if (fabs(difference.y) > GetHightDeviation(lockedAttacker))
+  {
+    printf(IM "To much height difference\n");
+    return false;
+  }
+
+  attackerRotation =
+    PT::Math::NormalizeAngle(attacker->getEntity()->getRotation());
+
+  if (difference.x == 0.0f) difference.x = PT_EPSILON;
+  attackAngle =
+    PT::Math::NormalizeAngle(atan2(difference.x, difference.z));
+
+  angleDiff = attackAngle - attackerRotation;
+
+  printf("attackerRotation: %f, attackAngle: %f angleDiff: %f allowedAngle: %f\n",
+    attackerRotation, attackAngle, angleDiff, allowedAngle);
+
+  if (fabs(angleDiff) > allowedAngle)
+  {
+    printf(IM "Not facing the target\n");
+    return false;
+  }
+
+  return true;
 }
 
 bool InteractionManager::SelectTarget(const PcEntity *sourceEntity,
