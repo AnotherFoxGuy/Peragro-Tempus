adc23be51f5f2a635ea6e08a0d931293913694c9
mononoke <>
2009-01-09 21:09:32 +0000

    Adding missing #include<stdlib.h>

--- src/server/combat/progressionmanager.cpp
+++ src/server/combat/progressionmanager.cpp
@@ -16,6 +16,7 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+#include <stdlib.h>
 #include "server/entity/entitymanager.h"
 #include "server/entity/statmanager.h"
 #include "interactionutility.h"
