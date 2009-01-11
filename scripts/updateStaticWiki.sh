38635dd55b9856e3c9f45461b8547229c6bd6f6f
cyanox <>
2009-01-11 14:03:18 +0000

    script for automatic wiki export into svn.

---
+++ scripts/updateStaticWiki.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+DOCS=/var/www/sites/peragro.nl_wiki/HTML/static
+DEST=/home/peragro/peragro/docs/
+
+cd $DEST
+
+rm -rf $DEST/static
+rsync -r $DOCS $DEST
+FILES=`find static/ -name .svn -prune -o -print`
+svn add static -N
+for i in $FILES
+do
+  svn add $i -N
+done
+
+svn status | grep "\!" | awk ' { print $2 } ' | xargs svn del
+
+svn ci . -m "Automated static wiki export update."
