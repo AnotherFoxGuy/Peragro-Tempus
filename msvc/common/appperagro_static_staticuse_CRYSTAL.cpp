98c14ed752c38c14d17064d4145a9afe120c3dd8
PTServerSystem <>
2010-02-21 04:05:32 +0000

    Automated MSVC project file regeneration.

--- msvc/common/appperagro_static_staticuse_CRYSTAL.cpp
+++ msvc/common/appperagro_static_staticuse_CRYSTAL.cpp
@@ -98,6 +98,4 @@ SCF_USE_STATIC_PLUGIN(sndsyswav)
 SCF_USE_STATIC_PLUGIN(sndmanager)
 SCF_USE_STATIC_PLUGIN(sndsysnull)
 SCF_USE_STATIC_PLUGIN(sndsyssoft)
-SCF_USE_STATIC_PLUGIN(sndsysopenal)
-SCF_USE_STATIC_PLUGIN(sndsyswin)
 
